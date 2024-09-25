4f2e5ab2ef1833d1ed1eff3b20439bfdae74c90b
krish2898ad <kirankrishna2k4@gmail.com>
2024-09-25 21:35:13 +0530

    frst commit

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,17 +1,51 @@
- /* GET 'home' page */
- const homelist = (req, res) => {
-    res.render('locations-list', { title: 'Home' });
-   };
-   /* GET 'Location info' page */
-   const locationInfo = (req, res) => {
+const homelist = (req, res) => {
+  res.render('locations-list', {
+      title: 'Loc8r - find a place to work with wifi',
+      pageHeader: {
+          title: 'Loc8r',
+          strapline: 'Find places to work with wifi near you!'
+      },
+      sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+      locations: [
+          {
+              name: 'GOA',
+              address: 'Goa: Where Every Shore Tells a Story!',
+              rating: 3,
+              price: 12000,
+              facilities: ['Stay', 'Food', 'Transport'],
+              distance: '5D Trip'
+          },
+          {
+              name: 'Manali',
+              address: 'Manali: Your Gateway to Nature Majesty!',
+              rating: 4,
+              price: 24000,
+              facilities: ['Premium Hotel Stay', 'Food', 'Travel Guide'],
+              distance: '2W Trip'
+          },
+          {
+              name: 'Ooty',
+              address: 'Ooty: The Queen of Hill Stations Awaits!',
+              rating: 2,
+              price: 8000,
+              facilities: ['Food', 'Stay'],
+              distance: '3D Trip'
+          }
+      ]
+  });
+};
+
+/* GET 'Location info' page */
+const locationInfo = (req, res) => {
     res.render('location-info', { title: 'Location info' });
    };
    /* GET 'Add review' page */
    const addReview = (req, res) => {
     res.render('location-review-form', { title: 'Add review' });
    };
-   module.exports = {
-    homelist,
-    locationInfo,
-    addReview
-   };
+
+module.exports = {
+  homelist,
+  locationInfo,
+  addReview
+};
